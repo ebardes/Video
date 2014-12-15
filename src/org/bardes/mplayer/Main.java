a21ef488440aec7998762c7eac241c7f56c6728b
Eric Bardes <ericbardes@cchmc.org>
2014-12-15 18:08:17 -0500

    Major Frontend Work

--- src/org/bardes/mplayer/Main.java
+++ src/org/bardes/mplayer/Main.java
@@ -1,37 +1,6 @@
 package org.bardes.mplayer;
 
-/*
- * Copyright (c) 2011, Pro JavaFX Authors
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- * 3. Neither the name of JFXtras nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- *  FullScreenVideoPlayer.java - An example from the Pro JavaFX Platform book.
- */
-//package projavafx.fullscreenvideoplayer;
-
-import java.io.File;
+import java.net.URL;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -54,10 +23,11 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage)
 	{
-		File f = new File("main.fxml");
+		ClassLoader cl = getClass().getClassLoader();
+		URL url = cl.getResource("main.fxml");
 		try
 		{
-			Parent p = FXMLLoader.load(f.toURI().toURL());
+			Parent p = FXMLLoader.load(url);
 			Scene myScene = new Scene(p);
 			primaryStage.setScene(myScene);
 			primaryStage.show();
