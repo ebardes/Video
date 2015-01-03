65a9cade9e337986a2fdf2178c62ce0b5681ccf3
Eric Bardes <ericbardes@gmail.com>
2015-01-03 00:36:37 -0500

    Add thumbnail and release / remove buttons

--- src/org/bardes/mplayer/cue/CueLayerInfo.java
+++ src/org/bardes/mplayer/cue/CueLayerInfo.java
@@ -1,8 +1,14 @@
 package org.bardes.mplayer.cue;
 
+import org.bardes.mplayer.GroupSlot;
+import org.bardes.mplayer.Slot;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 public class CueLayerInfo
 {
@@ -20,7 +26,26 @@ public class CueLayerInfo
 
 	public int volumeValue;
 
-	private boolean changed = false;
+	public ChoiceBox<GroupSlot> group;
+	
+	public ChoiceBox<Slot> slot;
+	
+	public Pane cueView;
+	
+	private boolean touched = false;
+	
+	public void touch()
+	{
+	    if (!touched)
+	    {
+    	    ap.getStyleClass().clear();
+    	    ap.getStyleClass().add("modified");
+    	    touched = true;
+    	    
+    	    Button release = (Button) ap.lookup("#cueRelease");
+    	    release.setDisable(false);
+	    }
+	}
 
 	public void setDimmerLevel(Number newValue)
 	{
@@ -28,7 +53,7 @@ public class CueLayerInfo
 		String s = String.format("%d%%", val);
 		dimmerValue = val;
 		dimmerLabel.setText(s);
-		changed = true;
+		touch();
 	}
 
 	public void setVolumeLevel(Number newValue)
@@ -37,6 +62,23 @@ public class CueLayerInfo
 		String s = String.format("%d%%", val);
 		volumeValue = val;
 		volumeLabel.setText(s);
-		changed = true;
+		touch();
 	}
+
+    public void changeGroup(GroupSlot groupSlot)
+    {
+        slot.getItems().setAll(groupSlot.slots.values());
+        slot.getSelectionModel().selectFirst();
+        touch();
+    }
+
+    public void changeSlot(Slot newValue)
+    {
+        Slot s = slot.getValue();
+        cueView.getChildren().clear();
+        if (s != null)
+        {
+            cueView.getChildren().add(s.getThumbNail());
+        }
+    }
 }
