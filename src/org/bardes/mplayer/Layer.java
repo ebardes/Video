2f4d49f0a4168234265582448b81ddbd783ef291
Eric Bardes <ericbardes@gmail.com>
2015-02-01 20:39:20 -0500

    Support large footprint personality

--- src/org/bardes/mplayer/Layer.java
+++ src/org/bardes/mplayer/Layer.java
@@ -5,4 +5,6 @@ public interface Layer
 	void setDimmer(int intensity);
 	void setItem(int group, int slot);
 	void setVolume(int volume);
+    void shift(int xShift, int yShift, int xScale, int yScale, int rotate);
+    void setPlayMode(int playMode);
 }
