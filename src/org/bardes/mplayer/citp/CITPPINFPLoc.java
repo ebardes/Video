887be331e2604ffb9323dd27f8a18e21ec2617af
Eric Bardes <ericbardes@cchmc.org>
2015-02-03 17:20:28 -0500

    TCP CITP

--- src/org/bardes/mplayer/citp/CITPPINFPLoc.java
+++ src/org/bardes/mplayer/citp/CITPPINFPLoc.java
@@ -4,16 +4,19 @@ import java.nio.ByteBuffer;
 
 public class CITPPINFPLoc extends CITPPINFHeader
 {
-	public CITPPINFPLoc()
+	private int localPort;
+
+	public CITPPINFPLoc(int localPort)
 	{
 		super("PLoc");
+		this.localPort = localPort;
 	}
-	
+
 	@Override
 	public void stream(ByteBuffer bc)
 	{
 		super.stream(bc);
-		bc.putShort((short) 0);
+		bc.putShort((short) localPort);
 		string(bc, "MediaServer"); // Type
 		string(bc, ASTREAUS_MEDIA_SERVER); // Name
 		string(bc, "Disrepair");
