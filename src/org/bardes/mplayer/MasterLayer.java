7a381ab40e18e8e76757f19f1689a83ae1a05975
Eric Bardes <ericbardes@gmail.com>
2016-09-22 12:28:05 -0400

    Brightness / Contrast

--- src/org/bardes/mplayer/MasterLayer.java
+++ src/org/bardes/mplayer/MasterLayer.java
@@ -21,8 +21,8 @@ public class MasterLayer
         root.setTranslateX((double)(xShift - 32768) / 8.0);
         root.setTranslateY((double)(yShift - 32768) / 8.0);
         
-        root.setScaleX(xScale / 8192.0);
-        root.setScaleY(yScale / 8192.0);
+        root.setScaleX(xScale / 32768.0);
+        root.setScaleY(yScale / 32768.0);
         
         root.setRotate((double)(rotate - 32768) / 182.04); // 182.04 = 32768 / 180
     }
