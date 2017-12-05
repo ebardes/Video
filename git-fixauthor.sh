22d0dc5d125f5836517b0335fbe0ec2993e73c3a
Eric Bardes <ericbardes@gmail.com>
2017-12-05 12:05:39 -0500

    Added fix author script

---
+++ git-fixauthor.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="ericbardes@cchmc.org"
+CORRECT_NAME="Eric Bardes"
+CORRECT_EMAIL="ericbardes@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
